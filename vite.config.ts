7dea945d1936416b1b4c3a9a5e166cd81e69ca50
shu-illy <sh.illy.9117@gmail.com>
2025-03-01 03:20:50 +0900

    init TailwindCSS
    https://github.com/yusukebe/honox-examples/blob/main/projects/tailwind/vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,20 @@ import build from '@hono/vite-build/cloudflare-workers'
 import adapter from '@hono/vite-dev-server/cloudflare'
 import honox from 'honox/vite'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [
-    honox({
-      devServer: { adapter },
-      client: { input: ['./app/style.css'] }
-    }),
-    build()
-  ]
+export default defineConfig(() => {
+  return {
+    plugins: [
+      tailwindcss(),
+      honox({
+        devServer: { adapter },
+        client: { input: ['./app/style.css'] }
+      }),
+      build()
+    ],
+    ssr: {
+      external: ['@prisma/client'],
+    }
+  }
 })
